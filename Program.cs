abca4478f34f8797a47be1cee76773bb30ad3a13
Yeghor Danyliuk <egordan74@gmail.com>
2025-09-10 18:00:49 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Drawing;
+
+namespace PKPZlab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose x:");
+            float x = float.Parse(Console.ReadLine());
+            Console.WriteLine("Choose y:");
+            float y = float.Parse(Console.ReadLine());
+            Console.WriteLine("Choose Radius:");
+            float R = float.Parse(Console.ReadLine());
+            Console.WriteLine(Lab.InArea(x, y, R));
+        }
+    }
+    public static class Lab
+    {
+         public static string InArea(float x, float y, float R)
+        {
+            if (x < 0 && y > 0)
+            {
+                double point = (x * x + y * y);
+                if (point <= R * R)
+                {
+                    if (point == R * R)
+                    {
+                        return "On the EDGE!";
+                    }
+                    return "In the Area!";
+                }
+                return "Not on the Area!";
+            }
+            if (x > 0 && y < 0)
+            {
+                double yAC = -2 * x;
+                double yBC = 2 * (x - R);
+                if (y >= yAC && y >= yBC)
+                {
+                    if (y == yAC && y == yBC) { return "On EDGE!"; }
+                    return "On the Area!";
+                }
+            return "Not on the Area!";
+            }
+            return "Not on the Area!";
+        }
+    }
+}
+
