3ce923f23e0f9516c0ce0cd6e9dde3c4160c8b28
Yeghor Danyliuk <egordan74@gmail.com>
2025-09-11 23:22:45 +0300

    add chess part, comment all other programs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,31 @@
 ﻿using System.Drawing;
 
+
 namespace PKPZlab1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose x:");
-            float x = float.Parse(Console.ReadLine());
-            Console.WriteLine("Choose y:");
-            float y = float.Parse(Console.ReadLine());
-            Console.WriteLine("Choose Radius:");
-            float R = float.Parse(Console.ReadLine());
-            Console.WriteLine(Lab.InArea(x, y, R));
+            //Console.WriteLine("Choose x:");
+            //float x = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Choose y:");
+            //float y = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Choose Radius:");
+            //float R = float.Parse(Console.ReadLine());
+            //Console.WriteLine(Lab.InArea(x, y, R));
+            Console.WriteLine("Enter white king coordinates");
+            int[] wk = Chess.Input();
+            Console.WriteLine("Enter black horse coordinates");
+            int[] bh = Chess.Input();
+            Console.WriteLine("Enter black bishop coordinates");
+            int[] bb = Chess.Input();
+            if (Chess.CheckArrInent(wk,bh) || Chess.CheckArrInent(bh,bb) || Chess.CheckArrInent(wk,bb))
+            {
+                Console.WriteLine("Chesspieces are on the same x and y! retry!!!");
+                Main(args);
+            }
+            Chess.Chessplay(wk, bh, bb);
         }
     }
     public static class Lab
--- chess.cs
+++ chess.cs
@@ -4,9 +4,28 @@ namespace PKPZlab1
 {
     public static class Chess
     {
-        public static void Main(String[] args)
+        public static int[] Input()
         {
+            int[] outp = new int[2];
+            Console.WriteLine("Enter X:");
+            outp[0] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Y:");
+            outp[1] = int.Parse(Console.ReadLine());
+            if (outp[0] > 8 || outp[0] < 0 || outp[1] > 8 || outp[1] < 0)
+            {
+                Console.WriteLine("Entered wrong value! Start again");
+                Input();
+            }
+            return outp;
+        }
 
+        public static bool CheckArrInent(int[] arr1, int[] arr2)
+        {
+            if (arr1[0] == arr2[0] && arr1[1] == arr2[1])
+            {
+                return true;
+            }
+            return false;
         }
 
         public static bool HorseReach(int[] enemy, int[] horse)
@@ -39,26 +58,44 @@ namespace PKPZlab1
                 if (Math.Abs(white_king[0] - black_bishop[0]) == Math.Abs(white_king[1] - black_bishop[1]))
                 {
                     Console.WriteLine("Defence! white king kills black horse and black bishop kills the king!");
-                    return;
                 }
-                Console.WriteLine("Attack! white king kills black horse!");
-                return;
-            }
-            if ((Math.Abs(black_horse[0] - white_king[0]) == 1 || Math.Abs(black_horse[1] - white_king[1]) == 1)
+                else
                 {
+                    Console.WriteLine("Attack! white king kills black horse!");
+                }
+            }
+            if ((Math.Abs(black_horse[0] - white_king[0]) == 1 || Math.Abs(black_horse[1] - white_king[1]) == 1))
+            {
                 // king kills bishop//
-                white_king = black_bishop;
-                if (Chess.HorseReach(white_king, black_horse))
+                int[] newCor = white_king;
+                newCor = black_bishop;
+                if (Chess.HorseReach(newCor, black_horse))
                 {
-                Console.WriteLine("Attack! white king kills black bishop and black horse kills the king!");
-                return;
+                    Console.WriteLine("Attack! white king kills black bishop and black horse kills the king!");
+                }
+                else 
+                { 
+                    Console.WriteLine("Nothing happens, just regular move");
                 }
             }
-            Console.WriteLine("Nothing happens, just regular move");
             Console.WriteLine("If black horse moves first \n");
-
+            if (HorseReach(white_king, black_horse))
+            {
+                Console.WriteLine("Attack! Black horse kills white king!");
+            }
+            else
+            {
+                Console.WriteLine("Nothing happens, regular move.");
+            }
+            Console.WriteLine("If black bishop moves first");
+            if (Math.Abs(white_king[0] - black_bishop[0]) == Math.Abs(white_king[1] - black_bishop[1]))
+            {
+                Console.WriteLine("Attack! Black Bishop kills white king!");
+                return;
+            }
+            Console.WriteLine("Nothing happens. Regular move.");
         }
-        }
+    }
 
 }
 
